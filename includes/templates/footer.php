f358dc148ed6e1a86f308e2a08f57fc55683a789
Agustin <aguscontreras@outlook.com.ar>
2019-10-06 22:18:26 -0300

    Modificado parte footer

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -1,34 +1,62 @@
 <footer class="site-footer">
-  <div class="contenedor clearfix">
-    <div class="footer-informacion">
-      <h3>Sobre <span>GDLWEBCAMP</span></h3>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci voluptates veritatis delectus eveniet
-        nesciunt amet dolore voluptatem est laboriosam vitae aspernatur harum obcaecati natus qui alias sit, dolor
-        eaque laudantium!
-      </p>
+    <div class="contenedor clearfix">
+        <div class="footer-informacion">
+            <h3>Sobre <span>GDLWEBCAMP</span></h3>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci voluptates veritatis delectus eveniet
+                nesciunt amet dolore voluptatem est laboriosam vitae aspernatur harum obcaecati natus qui alias sit, dolor
+                eaque laudantium!
+            </p>
+        </div>
+        <div class="ultimos-tweets">
+            <h3>Últimos <span>Tweets</span></h3>
+            <ul>
+                <li>Sit consectetur aute labore id exercitation ullamco sint ut ipsum ullamco nisi quis.</li>
+                <li>Culpa incididunt consectetur tempor incididunt laborum magna veniam.</li>
+                <li>Dolor aliqua adipisicing pariatur commodo aute aliqua eu exercitation do.</li>
+            </ul>
+        </div>
+        <div class="menu">
+            <h3>Redes <span>Sociales</span></h3>
+            <nav class="redes-sociales">
+                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-pinterest-p"></i></a>
+                <a href="#"><i class="fab fa-youtube"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+            </nav>
+        </div>
     </div>
-    <div class="ultimos-tweets">
-      <h3>Últimos <span>Tweets</span></h3>
-      <ul>
-        <li>Sit consectetur aute labore id exercitation ullamco sint ut ipsum ullamco nisi quis.</li>
-        <li>Culpa incididunt consectetur tempor incididunt laborum magna veniam.</li>
-        <li>Dolor aliqua adipisicing pariatur commodo aute aliqua eu exercitation do.</li>
-      </ul>
-    </div>
-    <div class="menu">
-      <h3>Redes <span>Sociales</span></h3>
-      <nav class="redes-sociales">
-        <a href="#"><i class="fab fa-facebook-f"></i></a>
-        <a href="#"><i class="fab fa-twitter"></i></a>
-        <a href="#"><i class="fab fa-pinterest-p"></i></a>
-        <a href="#"><i class="fab fa-youtube"></i></a>
-        <a href="#"><i class="fab fa-instagram"></i></a>
-      </nav>
-    </div>
-  </div>
 
-  <p class="copyright">
-    Todos los derechos reservados GDLWEBCAMP 2018.
-  </p>
+    <p class="copyright">
+        Todos los derechos reservados GDLWEBCAMP 2018.
+    </p>
 </footer>
+
+<script src="js/vendor/modernizr-3.6.0.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<script>
+    window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')
+</script>
+<script src="js/plugins.js"></script>
+<script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js"></script>
+<script src="js/jquery.countdown.min.js"></script>
+<script src="js/jquery.animateNumber.min.js"></script>
+<script src="js/jquery.lettering.js"></script>
+<script src="js/jquery.waypoints.min.js"></script>
+<script src="js/main.js"></script>
+
+<!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+<script>
+    window.ga = function() {
+        ga.q.push(arguments)
+    };
+    ga.q = [];
+    ga.l = +new Date;
+    ga('create', 'UA-XXXXX-Y', 'auto');
+    ga('send', 'pageview')
+</script>
+<script src="https://www.google-analytics.com/analytics.js" async defer></script>
+</body>
+
+</html>
