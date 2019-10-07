3df0c82fead1f5f746d20f3527f448fee668924d
Agustin <aguscontreras@outlook.com.ar>
2019-10-06 22:17:59 -0300

    Creado calendario

---
+++ calendario.php
@@ -0,0 +1,9 @@
+<?php include_once 'includes/templates/header.php'; ?>
+
+  <section class="seccion contenedor">
+    <h2>Calendario de Eventos</h2>
+
+  </section>
+  <!-- .seccion -->
+
+  <?php include_once 'includes/templates/footer.php'; ?>
