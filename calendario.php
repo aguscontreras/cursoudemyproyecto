6a6f17dff29868a5d4ed06bdedcdac596596d659
Agustin <aguscontreras@outlook.com.ar>
2019-10-06 22:29:28 -0300

    Conexion a bbdd

--- calendario.php
+++ calendario.php
@@ -1,9 +1,37 @@
 <?php include_once 'includes/templates/header.php'; ?>
 
-  <section class="seccion contenedor">
+<section class="seccion contenedor">
+
     <h2>Calendario de Eventos</h2>
 
-  </section>
-  <!-- .seccion -->
+    <?php
+    try {
+        //code...
+        require_once('includes/funciones/bd_conexion.php');
+        $sql = 'SELECT * FROM eventos';
+        $resultado = $conn->query($sql);
+    } catch (\Exception $e) {
+        //throw $th;
+        echo $e->getMessage();
+    }
+    ?>
 
-  <?php include_once 'includes/templates/footer.php'; ?>
+    <div class="calendario">
+        <?php
+        $eventos = $resultado->fetch_assoc();
+        ?>
+
+        <pre>
+        <?php var_dump($eventos); ?>
+    </pre>
+
+    </div>
+
+    <?php
+    $conn->close();
+    ?>
+
+</section>
+<!-- .seccion -->
+
+<?php include_once 'includes/templates/footer.php'; ?>
