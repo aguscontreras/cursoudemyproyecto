58ba1c7977511c913fc12671538af59fc4c14c1a
Agustin <aguscontreras@outlook.com.ar>
2019-10-15 09:43:26 -0300

    Agregado codificacion utf8

--- includes/funciones/bd_conexion.php
+++ includes/funciones/bd_conexion.php
@@ -1,5 +1,6 @@
 <?php
     $conn = new mysqli('192.168.1.12', 'gdlweb', 'Egws37851', 'gdlwebcamp');
+    $conn->query("SET NAMES 'utf8' ");
     if ($conn->connect_error) {
         echo $error->$conn->connect_error;
     }
