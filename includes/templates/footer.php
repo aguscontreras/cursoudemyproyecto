83d716b479364b82e69ea322fd683cfeee271c39
Agustin <aguscontreras@outlook.com.ar>
2019-10-21 01:27:34 -0300

    Modificado condicional

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -48,7 +48,7 @@
 <?php
 $archivo = basename($_SERVER{'PHP_SELF'});
 $pagina = str_replace(".php", "", $archivo);
-if ($pagina == 'invitados') {
+if ($pagina == 'invitados' || $pagina == 'index') {
     echo '<script src="js/jquery.colorbox.js"></script>';
 } else if ($pagina == 'conferencia') {
     echo '<script src="js/lightbox.js"></script>';
