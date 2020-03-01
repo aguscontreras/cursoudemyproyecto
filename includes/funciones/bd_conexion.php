953577e95abbd392db1a8cc940a0c03de878fb21
Agustin <aguscontreras@outlook.com.ar>
2020-03-01 17:48:33 -0300

    Adaptado a Hostinger

--- includes/funciones/bd_conexion.php
+++ includes/funciones/bd_conexion.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = new mysqli('192.168.1.12', 'gdlweb', 'Egws37851', 'gdlwebcamp');
+    $conn = new mysqli('localhost', 'id12774087_gdlweb', 'Egws37851', 'id12774087_gdlwebcamp');
     $conn->query("SET NAMES 'utf8' ");
     if ($conn->connect_error) {
         echo $error->$conn->connect_error;
