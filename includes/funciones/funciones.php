9177aeae01e8f766be7fcc4d56815fa37fbf1494
Agustin <aguscontreras@outlook.com.ar>
2020-02-29 23:05:34 -0300

    Terminado registro

--- includes/funciones/funciones.php
+++ includes/funciones/funciones.php
@@ -16,8 +16,27 @@ function productos_json (&$boletos, &$camisas = 0, &$etiquetas = 0) {
         }
     }
 
+    $camisas = (int) $camisas;
+    if ($camisas > 0) {
+        $json['camisas'] = $camisas;
+    }
+
+    $etiquetas = (int) $etiquetas;
+    if ($etiquetas > 0) {
+        $json['etiquetas'] = $etiquetas;
+    }
+
     return json_encode($json);
 
 };
+
+function eventos_json (&$eventos) {
+    $eventos_json = array();
+    foreach ($eventos as $evento) {
+        $eventos_json['eventos'][] = $evento;
+    }
+
+    return json_encode($eventos_json);
+}
 
 ?>
