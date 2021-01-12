1b2f23ef61668b10bb2a6d1d724bb03b88ff85cf
aguscontreras <35529189+aguscontreras@users.noreply.github.com>
2021-01-12 20:34:27 -0300

    Delete bd_conexion.php

--- includes/funciones/bd_conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-    // $conn = new mysqli('sql10.freemysqlhosting.net:3306', 'sql10325113', 'ZppDQdpz2b', 'sql10325113');
-    $conn = new mysqli('aguscontdev.ddns.net', 'gdlweb', 'Egws37851', 'gdlwebcamp');
-    $conn->query("SET NAMES 'utf8' ");
-    if ($conn->connect_error) {
-        echo $error->$conn->connect_error;
-    }
-?>
